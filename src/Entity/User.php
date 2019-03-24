273fdae3041b5d2a47eba33566ee29e727c7f73a
eosipov <zheka0168@gmail.com>
2019-03-24 06:13:23 +0400

    added entity and test row in DB

---
+++ src/Entity/User.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $role_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $room_id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $token = null;
+
+    /**
+     * @ORM\Column(type="integer", length=20, nullable=true)
+     */
+    private $money = 0;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rang = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRoleId(): ?int
+    {
+        return $this->role_id;
+    }
+
+    public function setRoleId(int $role_id): self
+    {
+        $this->role_id = $role_id;
+
+        return $this;
+    }
+
+    public function getRoomId(): ?int
+    {
+        return $this->room_id;
+    }
+
+    public function setRoomId(int $room_id): self
+    {
+        $this->room_id = $room_id;
+
+        return $this;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(?string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getMoney(): ?string
+    {
+        return $this->money;
+    }
+
+    public function setMoney(?string $money): self
+    {
+        $this->money = $money;
+
+        return $this;
+    }
+
+    public function getRang(): ?int
+    {
+        return $this->rang;
+    }
+
+    public function setRang(int $rang): self
+    {
+        $this->rang = $rang;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190324020601.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190324020601 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, role_id INTEGER NOT NULL, room_id INTEGER NOT NULL, login VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, token VARCHAR(255) DEFAULT NULL, money INTEGER DEFAULT NULL, rang INTEGER DEFAULT 0 NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649AA08CB10 ON user (login)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE user');
+    }
+}
